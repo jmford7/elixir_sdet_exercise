ff2d5e7c3b6e47632906a0874f7e3d561c18ae51
Justin Ford <jmford7@gmai.com>
2019-07-21 09:15:36 -0600

    Initial commit

---
+++ elixir_sdet_exercise/_build/test/lib/elixir_sdet_exercise/ebin/elixir_sdet_exercise.app
@@ -0,0 +1,6 @@
+{application,elixir_sdet_exercise,
+             [{applications,[kernel,stdlib,elixir,logger,hound]},
+              {description,"elixir_sdet_exercise"},
+              {modules,['Elixir.ElixirSdetExercise']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
--- elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
+++ elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
@@ -6,8 +6,17 @@ defmodule ElixirSdetExerciseTest do
   # Start hound session and destroy when tests are run
   hound_session()
 
-  test "goes to google" do
-    navigate_to "http://google.com"
+  test "goes to facebook" do
+    navigate_to "http://facebook.com"
     IO.inspect page_title()
+    assert page_title() == "Facebook - Log In or Sign Up"
+    fill_field({:css, "input[name='firstname']"}, "Trial")
+
+    # click({:css, 'input[name="firstname"]'})
+    # send_text("Trial")
+    # take_screenshot("C:/Users/jmfor/Desktop/Divvy_Challenge/screenshots")
   end
+  # test "verify facebook open" do
+
+  # end
 end
